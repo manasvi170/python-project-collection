8efaf40afc45480bfef4e3c05bbf9859375dfa57
Manasvi Sood <manasvisood403@gmail.com>
2026-06-27 15:34:21 +0530

    Add Secret Auction program to project

--- secret-auction-program/main.py
+++ secret-auction-program/main.py
@@ -5,22 +5,22 @@ def find_highest_bidder(bidding_record):
     highest_bid = 0
     winner = ""
     for bidder in bidding_record:
-        bid_amount = bidding_record[bidder]  #Retrieving the values from Dictionary dict={Key:Value}
+        bid_amount = bidding_record[bidder]
         if bid_amount > highest_bid:
             highest_bid = bid_amount
             winner = bidder
     print(f"The winner is {winner} with a bid of ${highest_bid}")
 
 
-bids={}
-continue_bidding=True
+bids = {}
+continue_bidding = True
 while continue_bidding:
-    name = input("What is your name ?\t").capitalize()
-    price = input("How much you want to bid ?\t $")
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
     bids[name] = price
-    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n").lower()
-    if should_continue=="no":
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        continue_bidding = False
         find_highest_bidder(bids)
-        continue_biding=False
-    elif should_continue=="yes":
-        print("\n" * 100)
+    elif should_continue == "yes":
+        print("\n" * 20)
